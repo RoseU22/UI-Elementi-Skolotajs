099b54db35cd4eae35526b804105841f2a6d98d3
RozeU <rozeu668@gmail.com>
2023-04-17 16:10:55 +0300

    Uzsakta ainu parslegsanas funkcionalitates izstrade

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Importe, lai varetu darboties ar ainam
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+
+	public void UzSakumu () {
+		SceneManager.LoadScene ("Sakums", LoadSceneMode.Single);
+	}
+
+	public void UzBinaAinu(){
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+	}
+
+	public void Apturet(){
+		Application.Quit();
+	}
+}
--- Assets/Skripti/ObjektuParvietosana.cs
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//importe, lai varetu darboties ar pointer interfeisu
+using UnityEngine.EventSystems;
 
-public class ObjektuParvietosana : MonoBehaviour {
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
+			 IDragHandler, IEndDragHandler{
+
+	public Canvas kanva;
+	private RectTransform transformacijaslogs;
 
 	// Use this for initialization
 	void Start () {
-		
+		transformacijaslogs = GetComponent<RectTransform>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log("Izdarits kreisais klikskis uz velkama objekta!");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log("Uzsakta objekta vilksana!");
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log("Notiek objekta vilksana!");
+		transformacijaslogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log("Beigta objekta vilksana!");
 	}
 }
